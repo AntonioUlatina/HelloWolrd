c9eddf8cf760a97023b57b88b4941cb9a93dcd13
AntonioUlatina <36631557+AntonioUlatina@users.noreply.github.com>
2018-11-11 13:40:16 -0600

    Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public void hi() {
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld test = new HelloWorld();
+		test.hi();
+	}
+
+}
