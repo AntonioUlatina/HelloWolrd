8345d54211d07fc40fbb5a91c1f96c6b4294a0ae
AntonioUlatina <36631557+AntonioUlatina@users.noreply.github.com>
2018-11-29 20:55:11 -0600

    nov 29

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,26 @@
 
 public class HelloWorld {
 
-	public void hi() {
-		System.out.println("Hello World");
+	public String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void whatsMyName() {
+		System.out.println("My name is what? My name is what? My name is " + name);
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		HelloWorld test = new HelloWorld();
-		test.hi();
+		System.out.println("Hello World!");
+		HelloWorld hw = new HelloWorld();
+		hw.setName("Antonio");
+		hw.whatsMyName();
+
 	}
 
 }
